da6c9f327d8270b0b8e7928db90b996c1640c611
kdarrey <daniel.rey.rey@gmail.com>
2020-06-27 15:49:48 +0200

    'init'

---
+++ src/main/java/com/hack/equipo4/Equipo4Application.java
@@ -0,0 +1,13 @@
+package com.hack.equipo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hack/equipo4/Equipo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.equipo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
