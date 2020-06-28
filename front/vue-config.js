f76f710e3741a33e73605b9ae7c0bdaa01246300
artellador <info@artellando.com>
2020-06-28 05:29:21 +0200

    Engadida configuración vue-cli

---
+++ front/vue-config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+function resolveSrc(_path) {
+  return path.join(__dirname, _path);
+}
+// vue.config.js
+module.exports = {
+  lintOnSave: true,
+  publicPath: './',
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    resolve: {
+      alias: {
+        assets: resolveSrc('src/assets')
+      }
+    },
+    devtool: process.env.NODE_ENV === 'development' ? 'source-map': ''
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production',
+    loaderOptions: {
+      css: {
+         url: false,
+        }
+    }    
+  }
+};
