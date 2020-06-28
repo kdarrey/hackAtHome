c5cbbe92d56ae6be30902a7422f7267f7b9f3018
artellador <info@artellando.com>
2020-06-28 05:31:51 +0200

    Configurada Login como pantalla de inicio Creada Home como pantalla de destino tras autenticacion

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Créditos</h1>
+    <div>
+      <ul>
+        <li>
+          Image by
+          <a
+            href="https://pixabay.com/users/Wounds_and_Cracks-218774/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=411975"
+            >Christos Giakkas</a
+          >
+          from
+          <a
+            href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=411975"
+            >Pixabay</a
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
---
+++ front/src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="login-bg d-flex flex-column vh-100">
+    <div class="m-auto">
+      <h1 class="text-center"><strong>eFeira</strong></h1>
+      <el-card class="login-card mt-5">
+        <div class="text-center m-3">
+          <font-awesome-icon icon="user-circle" size="6x" />
+        </div>
+        <el-form :model="form">
+          <el-form-item>
+            <el-input
+              name="username"
+              placeholder="Nombre de usuario"
+              v-model="form.username"
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              name="password"
+              placeholder="Contraseña"
+              v-model="form.password"
+            />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="authenticate" class="w-100"
+              >Acceder</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+<style scoped>
+.login-card {
+  min-width: 300px;
+}
+
+.login-bg {
+  background-image: url("/img/potatoes-411975_1920.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserCircle);
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    authenticate() {
+      this.$router.replace({ name: "Home" });
+    }
+  }
+};
+</script>
